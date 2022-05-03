a1ab5b0c5c5def995dd8d2e2dc6e4056bcc2d144
Leonardo Jose Nunes <leonardo.ferax@gmail.com>
2022-05-03 14:17:53 -0300

    Recipe List Component

---
+++ src/components/RecipeList.js
@@ -0,0 +1,20 @@
+import "./RecipeList.css"
+import { Link } from "react-router-dom"
+
+const RecipeComponent = ({ recipes }) => {
+  return (
+    <div className="recipe-list">
+      {recipes.map(recipe => (
+        <div className="card" key={recipe.id}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.cookinTime} to make.</p>
+
+          <div className="">{recipe.method.substring(0, 100)} ...</div>
+          <Link to={`/recipes/${recipe.id}`}>Cook This</Link>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default RecipeComponent
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,6 +1,9 @@
 import { useFetch } from "../../hooks/useFetch"
 import "./Home.css"
 
+// componets
+import RecipeList from "../../components/RecipeList"
+
 const Home = props => {
   const { data, isPending, error } = useFetch("http://localhost:3000/recipes")
 
@@ -8,7 +11,7 @@ const Home = props => {
     <div className="home">
       {error && <p className="error">{error}</p>}
       {isPending && <p className="loading">Loading...</p>}
-      {data && data.map(recipe => <h2 key={recipe.id}>{recipe.title}</h2>)}
+      {data && <RecipeList recipes={data} />}
     </div>
   )
 }
