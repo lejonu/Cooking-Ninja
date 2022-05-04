c744cc8c1656b4ec5a34c9322a4330a2337a1394
Leonardo Jose Nunes <leonardo.ferax@gmail.com>
2022-05-04 09:58:37 -0300

    Making a POST Request

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,19 +1,33 @@
 import { useState, useEffect } from "react"
 
-export const useFetch = (url) => {
+export const useFetch = (url, method = "GET") => {
   const [data, setData] = useState(null)
   const [isPending, setIsPending] = useState(false)
   const [error, setError] = useState(null)
+  const [options, setOptions] = useState(null)
+
+  const postData = postData => {
+    setOptions({
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(postData)
+    })
+  }
 
   useEffect(() => {
     const controller = new AbortController()
 
-    const fetchData = async () => {
+    const fetchData = async fetchOptions => {
       setIsPending(true)
-      
+
       try {
-        const res = await fetch(url, { signal: controller.signal })
-        if(!res.ok) {
+        const res = await fetch(url, {
+          ...fetchOptions,
+          signal: controller.signal
+        })
+        if (!res.ok) {
           throw new Error(res.statusText)
         }
         const data = await res.json()
@@ -26,18 +40,23 @@ export const useFetch = (url) => {
           console.log("the fetch was aborted")
         } else {
           setIsPending(false)
-          setError('Could not fetch the data')
+          setError("Could not fetch the data")
         }
       }
     }
 
-    fetchData()
+    if (method === "GET") {
+      fetchData()
+    }
+
+    if (method === "POST" && options) {
+      fetchData(options)
+    }
 
     return () => {
       controller.abort()
     }
+  }, [url, options, method])
 
-  }, [url])
-
-  return { data, isPending, error }
+  return { data, isPending, error, postData }
 }
--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,4 +1,5 @@
-import { useState } from "react"
+import { useState, useRef } from "react"
+import { useFetch } from "../../hooks/useFetch"
 import "./Create.css"
 
 import React from "react"
@@ -7,11 +8,38 @@ const Create = props => {
   const [title, setTitle] = useState("")
   const [method, setMethod] = useState("")
   const [cookingTime, setCookingTime] = useState("")
+  const [newIngredient, setNewIngredient] = useState("")
+  const [ingredients, setIngredients] = useState([])
+  const ingredientInput = useRef(null)
+
+  const { postData, data, error } = useFetch(
+    "http://localhost:3000/recipes",
+    "POST"
+  )
 
   const handleSubmit = e => {
     e.preventDefault()
 
-    console.log(title, method, cookingTime)
+    console.log(title, method, cookingTime, ingredients)
+
+    postData({
+      title,
+      ingredients,
+      method,
+      cookingTime: cookingTime + " minutes"
+    })
+  }
+
+  const handleAdd = e => {
+    e.preventDefault()
+    const ing = newIngredient.trim()
+
+    if (ing && !ingredients.includes(ing)) {
+      setIngredients(prevIngridients => [...prevIngridients, ing])
+    }
+
+    setNewIngredient("")
+    ingredientInput.current.focus()
   }
 
   return (
@@ -30,6 +58,26 @@ const Create = props => {
         </label>
 
         {/* ingredients goes here */}
+
+        <label>
+          <span>Recipe ingredients: </span>
+          <div className="ingredients">
+            <input
+              type="text"
+              onChange={e => setNewIngredient(e.target.value)}
+              value={newIngredient}
+              ref={ingredientInput}
+            />
+            <button onClick={handleAdd} className="btn">
+              add
+            </button>
+          </div>
+        </label>
+
+        <p>
+          Current ingredients:{" "}
+          {ingredients && ingredients.map(i => <em key={i}>{i}, </em>)}
+        </p>
 
         <label>
           <span>Recipe method:</span>
